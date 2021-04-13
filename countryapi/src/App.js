60d8669fac7fc3ae54fa79820d4d5441cb6949d2
Hilda Amponsah <hilda.amp@hotmail.com>
2021-04-13 19:26:56 +0100

    created constructor function, defining state- eu, asia which will be fetching info from api

--- countryapi/src/App.js
+++ countryapi/src/App.js
@@ -2,13 +2,24 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+    asias: [],
+    eupopes: [],
+    isLoaded: false
+  }
+}
+
   render() {
     return (
         <div>
-  
+        <h1>Countries</h1>
       </div>
     );
   }
 }
 
 export default App;
+
