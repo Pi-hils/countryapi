88cd3c8bbb295c21d63533dd3946dec22a13779b
Hilda Amponsah <hilda.amp@hotmail.com>
2021-04-14 18:12:51 +0100

    changed the name of the component, included choices, added value instead of region to output the country

---
+++ countryapi/src/components/DropdownSelector.js
@@ -0,0 +1,53 @@
+import React from 'react';
+
+
+class DropdownSelector extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    isLoaded: false,
+    value: '',
+    choices: ['asia', 'europe']
+  };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    const { value } = this.state
+    const { onChoiceSelected } = this.props
+    
+    event.preventDefault();
+    onChoiceSelected(value)
+  }
+
+  render() {
+    var { isLoaded } = this.state;
+
+    if (!isLoaded) {
+      return(<div className='form'>
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Please select Region:
+          <select value={this.state.value} onChange={this.handleChange}>
+          
+            <option value="asia">Asia</option>
+            <option value="europe">Europe</option>
+           
+          </select>
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+      </div>
+      )
+    }
+  
+  }
+}
+
+export default DropdownSelector
